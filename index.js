15c3d5bc013935ba11618c8d6fba79ddd2f01b22
galois1423 <galois1423@gmail.com>
2023-01-15 14:49:49 +0900

    debug 2

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ const show = c => {
 
         var t = document.createElement('a')
         t.setAttribute('class', 'taglink')
-        t.addEventListener('click',location.href = '/wlog/search?tag='+tags[codes.indexOf(c[i])][0])
+        t.addEventListener('click',tagsearch(tags[codes.indexOf(c[i])][0]))
         t.innerHTML = tags[codes.indexOf(c[i])][0]
         ts.appendChild(t)
 
         for(var j=1; j<tags[codes.indexOf(c[i])].length; j++){
             var t = document.createElement('a')
             t.setAttribute('class', 'taglink')
-            t.addEventListener('click',location.href = '/wlog/search?tag='+tags[codes.indexOf(c[i])][j])
+            t.addEventListener('click',tagsearch(tags[codes.indexOf(c[i])][j]))
             t.innerHTML = '> '+tags[codes.indexOf(c[i])][j]
             ts.appendChild(t)
         }
