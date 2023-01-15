95b3d4c555deac6e3863d23cf138c0c95fd1c288
galois1423 <galois1423@gmail.com>
2023-01-15 15:00:59 +0900

    debug 4

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import {titles, tags, codes} from './index_data.js'
 
-const tagsearch = s =>{
-    location.href = '/wlog/search?tag='+s
-}
-
 const show = c => {
     var l = document.getElementById('list')
     for(var i=0; i<c.length;i++){
@@ -21,14 +17,14 @@ const show = c => {
 
         var t = document.createElement('a')
         t.setAttribute('class', 'taglink')
-        t.addEventListener('click',function(event){tagsearch(tags[codes.indexOf(c[i])][0])})
+        t.setAttribute('href','/wlog/search?tag='+tags[codes.indexOf(c[i])][0])
         t.innerHTML = tags[codes.indexOf(c[i])][0]
         ts.appendChild(t)
 
         for(var j=1; j<tags[codes.indexOf(c[i])].length; j++){
             var t = document.createElement('a')
             t.setAttribute('class', 'taglink')
-            t.addEventListener('click',function(event){tagsearch(tags[codes.indexOf(c[i])][j])})
+            t.setAttribute('href','/wlog/search?tag='+tags[codes.indexOf(c[i])][j])
             t.innerHTML = '> '+tags[codes.indexOf(c[i])][j]
             ts.appendChild(t)
         }
